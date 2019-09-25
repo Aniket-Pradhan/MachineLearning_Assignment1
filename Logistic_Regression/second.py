bf0283b2cde2aa421eef9e9eeb88b943750488c6
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-09-25 14:48:19 +0530

    mnsit logistic

--- Logistic_Regression/second.py
+++ Logistic_Regression/second.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import numpy as np
 from mlxtend.data import loadlocal_mnist
 from sklearn.linear_model import LogisticRegression
 
@@ -41,18 +42,88 @@ class second:
         print(self.X.shape, self.Y.shape)
         self.X = (self.X - self.X.mean())/self.X.std()
     
+    def load_testing_data(self):
+        self.X_test, self.Y_test = loadlocal_mnist(images_path=self.path.mnist + "/test_images", labels_path=self.path.mnist + "/test_labels")
+        self.X_test = (self.X_test - self.X_test.mean())/self.X_test.std()
+    
     def logistic_regression_train_ridge(self):
         print("Training ridge model")
         self.reg_ridge = LogisticRegression(penalty='l2', random_state=0, solver='saga', multi_class='ovr').fit(self.X, self.Y)
         self.pickle_save(self.reg_ridge, self.reg_ridge_name)
-        # print(reg_ridge.score(self.X, self.Y))
     
     def logistic_regression_train_lasso(self):
         print("Training lasso model")
         self.reg_lasso = LogisticRegression(penalty='l1', random_state=0, solver='saga', multi_class='ovr').fit(self.X, self.Y)
         self.pickle_save(self.reg_lasso, self.reg_lasso_name)
         # print(reg_ridge.score(self.X, self.Y))
+    
+    def train_error(self):
+        num_elements = self.X.shape[0]
+        num_correct_classifications_ridge = []
+        num_correct_classifications_lasso = []
+        num_classifications = []
+        for class_index in range(len(self.classes)):
+            num_classifications.append(0)
+            num_correct_classifications_ridge.append(0)
+            num_correct_classifications_lasso.append(0)
 
+        for element_index in range(num_elements):
+            train_X = self.X[element_index].reshape(1,-1)
+            train_Y = self.Y[element_index]
+            pred_ridge = self.reg_ridge.predict(train_X)
+            pred_lasso = self.reg_lasso.predict(train_X)
+            train_Y = int(train_Y)
+            if pred_ridge == train_Y:
+                num_correct_classifications_ridge[train_Y] += 1
+            if pred_lasso == train_Y:
+                num_correct_classifications_lasso[train_Y] += 1
+            num_classifications[train_Y] += 1
+        self.train_ridge_avg_list = []
+        self.train_lasso_avg_list = []
+        for class_index in range(len(self.classes)):
+            self.train_ridge_avg_list.append(num_correct_classifications_ridge[class_index]/num_classifications[class_index])
+            self.train_lasso_avg_list.append(num_correct_classifications_lasso[class_index]/num_classifications[class_index])
+        # print("average error:", self.train_ridge_avg_list)
+        # print("average error:", self.train_lasso_avg_list)
+    
+    def test_error(self):
+        num_elements = self.X_test.shape[0]
+        num_correct_classifications_ridge = []
+        num_correct_classifications_lasso = []
+        num_classifications = []
+        for class_index in range(len(self.classes)):
+            num_classifications.append(0)
+            num_correct_classifications_ridge.append(0)
+            num_correct_classifications_lasso.append(0)
+
+        for element_index in range(num_elements):
+            train_X = self.X_test[element_index].reshape(1,-1)
+            train_Y = self.Y_test[element_index]
+            pred_ridge = self.reg_ridge.predict(train_X)
+            pred_lasso = self.reg_lasso.predict(train_X)
+            train_Y = int(train_Y)
+            if pred_ridge == train_Y:
+                num_correct_classifications_ridge[train_Y] += 1
+            if pred_lasso == train_Y:
+                num_correct_classifications_lasso[train_Y] += 1
+            num_classifications[train_Y] += 1
+        self.test_ridge_avg_list = []
+        self.test_lasso_avg_list = []
+        for class_index in range(len(self.classes)):
+            self.test_ridge_avg_list.append(num_correct_classifications_ridge[class_index]/num_classifications[class_index])
+            self.test_lasso_avg_list.append(num_correct_classifications_lasso[class_index]/num_classifications[class_index])
+        # print("average error:", self.train_ridge_avg_list)
+        # print("average error:", self.train_lasso_avg_list)
+    
+    def pretty_print_accuracy(self):
+        num_elements = len(self.classes)
+        for ind in range(num_elements):
+            print("Training accuracy for class " + str(ind) + " for L1 regularization: ", self.train_lasso_avg_list[ind])
+            print("Training accuracy for class " + str(ind) + " for L2 regularization: ", self.train_ridge_avg_list[ind])
+            print("testing accuracy for class " + str(ind) + " for L1 regularization: ", self.test_lasso_avg_list[ind])
+            print("testing accuracy for class " + str(ind) + " for L2 regularization: ", self.test_ridge_avg_list[ind])
+            print()
+    
     def __init__(self):
         self.reg_ridge_name = "reg_ridge_model"
         self.reg_lasso_name = "reg_lasso_model"
@@ -64,6 +135,7 @@ class second:
         self.check_prev_models()
 
         self.load_training_data()
+        self.load_testing_data()
 
         if not self.check_ridge:
             self.logistic_regression_train_ridge()
@@ -73,6 +145,12 @@ class second:
             self.logistic_regression_train_lasso()
         else:
             self.reg_lasso = self.pickle_load(self.reg_lasso_name)
+        self.classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        self.train_error()
+        self.test_error()
+
+        self.pretty_print_accuracy()
 
 if __name__ == "__main__":
     second = second()
